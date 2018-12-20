e7c76421cdd27ee21d19b2f180dde4449de268a9
jonathanmunz <42818620+jonathanmunz@users.noreply.github.com>
2018-12-19 19:53:11 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,79 +4,10 @@
         <title> </title>
     </head>
     <body>
-        
-<a href="./labs/1">
-    Lab 1
-</a> <br>
-      
-<a href="./labs/2">
-    Lab 2
-</a> <br>
-
-<a href="https://cst336llab3.herokuapp.com/"> <!--Paste Mo's heroku lab3 link  -->
-    Lab 3
-</a> <br>
-
-<a href="./labs/4">
-    Lab 4
-</a> <br>
-
-<a href="./labs/5">
-    Lab 5
-</a> <br>
-
-<a href="./labs/6/welcome.php">
-    Lab 6
-</a> <br>
-
-<a href="./labs/7/index.php">
-    Lab 7
-</a> <br>
-
-<a href="./labs/8/index.html">
-    Lab 8
-</a> <br>
-
-<a href="./labs/9/ajaxsignup.php">
-    Lab 9
-</a> <br>
-
-<a href="./labs/10/index.php">
-    Lab 10
-</a> <br>
-
-<a href="./homework/1">
-    HW 1
-</a> <br>
-
-<a href="./homework/2/index.php">
-    HW 2
-</a> <br>
-
-<a href="./homework/3/index.php">
-    HW 3
-</a> <br>
-
-<a href="./homework/4/index.html">
-    HW 4
-</a> <br>
-
-<a href="https://cst336-final2018.herokuapp.com">
-    Final Project
-</a> <br>
-
-<a href="./practice/practicefinaljason/practice/index.html">
-    Practice Final
-</a> <br>
 
 <a href="./exams/Final/index.html">
     Final Exam
 </a> <br>
-
-<a href="./proseminar/eportfolio/aboutme.html">
-    Major Proseminar Eportfolio 
-</a> <br>
-
 
 
     </body>
